10c9dd2704e5c99ed184fda4dd29417d9e49e491
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-06 16:05:02 -0300

    Udemy POO Product

---
+++ application/ProgramProduct.java
@@ -0,0 +1,47 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramProduct {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product prod = new Product();
+
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        prod.name = sc.nextLine();
+
+        System.out.print("Price: ");
+        prod.price = sc.nextDouble();
+
+        System.out.print("Quantity in stock: ");
+        prod.quantity = sc.nextInt();
+
+        System.out.println();
+        System.out.println(prod.toString());
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        prod.addProducts(sc.nextInt());
+        System.out.println();
+
+        System.out.println(prod.toString());
+
+        System.out.print("Enter the number of products to be removed from stock: ");
+        int remov = sc.nextInt();
+        prod.removeProducts(remov);
+        System.out.println();
+
+        System.out.println(prod.toString());
+
+
+
+        sc.close();
+    }
+}
