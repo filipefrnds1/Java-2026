f53af643beb8a158d03fdf5735f5d4d22d20965d
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-12 02:45:53 -0300

    Java Matrix Exer

---
+++ exerciciosudemy/prograam/Aula111Exer.java
@@ -0,0 +1,52 @@
+package exerciciosudemy.prograam;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula111Exer {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int l = sc.nextInt();
+        int c = sc.nextInt();
+
+        int[][] mat = new int[l][c];
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                mat[i][j] = sc.nextInt();
+
+            }
+        }
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                if(n == mat[i][j]){
+                    System.out.println("Position " + i + "," + j + ":");
+                    if (j > 0) {
+                        System.out.println("Left: " + mat[i][j-1]);
+                    }
+                    if (i > 0) {
+                        System.out.println("Up: " + mat[i-1][j]);
+                    }
+                    if (j < mat[i].length-1) {
+                        System.out.println("Right: " + mat[i][j+1]);
+                    }
+                    if (i < mat.length-1) {
+                        System.out.println("Down: " + mat[i+1][j]);
+                    }
+
+                }
+
+            }
+        }
+
+
+
+
+        sc.close();
+    }
+}
