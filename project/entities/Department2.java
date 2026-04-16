66bfdaf51cb4c483971a1042e6ad01d476c1e5e2
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-16 14:58:41 -0300

    Department2

--- project/entities/Department2.java
+++ project/entities/Department2.java
@@ -1,4 +1,22 @@
 package project.entities;
 
 public class Department2 {
+
+    private String name;
+
+    public Department2(){
+
+    }
+
+    public Department2(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 }
