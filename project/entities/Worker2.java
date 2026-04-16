81de327f69b6547a3874a80b54f4cdfe85a99d7b
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-16 14:57:38 -0300

    Worker2

--- project/entities/Worker2.java
+++ project/entities/Worker2.java
@@ -1,4 +1,30 @@
 package project.entities;
 
+import project.entities.enums.WorkerLevel2;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Worker2 {
+    private String name;
+    private WorkerLevel2 level;
+    private Double baseSalary;
+
+    Department2 depart;
+    List<HourContract2> list = new ArrayList<>();
+
+    public Worker2(){
+
+    }
+
+    public Worker2(String name, WorkerLevel2 level, Double baseSalary, Department2 depart){
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.depart = depart;
+    }
+
+    
+
+
 }
