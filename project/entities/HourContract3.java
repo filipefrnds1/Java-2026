7a3b85dfd7e375c6ae9f0c1ee62d8074b85880d3
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-17 22:11:26 -0300

    HourContract3 Class

--- project/entities/HourContract3.java
+++ project/entities/HourContract3.java
@@ -1,4 +1,50 @@
 package project.entities;
 
+import java.time.LocalDate;
+
 public class HourContract3 {
+
+    private LocalDate date;
+    private Double valuePerHour;
+    private Integer hours;
+
+    public HourContract3(){
+
+    }
+
+    public HourContract3(LocalDate date, Double valuePerHour, Integer hours){
+        this.date = date;
+        this.valuePerHour = valuePerHour;
+        this.hours = hours;
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
+
+    public void setDate(LocalDate date){
+        this.date = date;
+    }
+
+    public Double getValuePerHour(){
+        return valuePerHour;
+    }
+
+    public void setValuePerHour(Double valuePerHour){
+        this.valuePerHour = valuePerHour;
+    }
+
+    public Integer getHours(){
+        return hours;
+    }
+
+    public void setHours(Integer hours){
+        this.hours = hours;
+    }
+
+    public Double totalValue(){
+        return valuePerHour * hours;
+    }
+
+    
 }
