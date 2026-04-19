564e8daa42c356cd1ac375fb19559449ebdab6e4
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-18 22:17:55 -0300

    Client Exer133

--- projectcomposi133/entities/Client.java
+++ projectcomposi133/entities/Client.java
@@ -1,4 +1,44 @@
 package projectcomposi133.entities;
 
+import java.time.LocalDateTime;
+
 public class Client {
+
+    private String name;
+    private String email;
+    private LocalDateTime birthDate;
+
+    public Client(){
+
+    }
+
+    public Client(String name, String email, LocalDateTime birthDate) {
+        setName(name);
+        setEmail(email);
+        setBirthDate(birthDate);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
 }
