e596cb6f32612a972ba999d245519ac57bfa094e
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-20 19:20:23 -0300

    OrderItem2 Exer133

--- projectcomposi133/entities/OrderItem2.java
+++ projectcomposi133/entities/OrderItem2.java
@@ -1,4 +1,58 @@
 package projectcomposi133.entities;
 
 public class OrderItem2 {
+
+    private Integer quantity;
+    private Double price;
+
+    private Product2 product2;
+
+    public OrderItem2(){
+
+    }
+
+    public OrderItem2(Integer quantity, Double price, Product2 product2) {
+        this.quantity = quantity;
+        this.price = price;
+        this.product2 = product2;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Product2 getProduct2() {
+        return product2;
+    }
+
+    public void setProduct2(Product2 product2) {
+        this.product2 = product2;
+    }
+
+    public double subTotal(){
+        return price * quantity;
+    }
+
+    @Override
+    public String toString(){
+        return getProduct2().getName()
+                + ", $"
+                + String.format("%.2f", price)
+                + ", Quantity: "
+                + quantity
+                + ", Subtotal: $"
+                + String.format("%.2f", subTotal());
+    }
 }
