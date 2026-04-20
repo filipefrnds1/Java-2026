7c9323c6cb3c42e0d35150a34927541da51a2f4f
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-20 19:19:32 -0300

    Cliente2 133

--- projectcomposi133/entities/Client2.java
+++ projectcomposi133/entities/Client2.java
@@ -1,4 +1,53 @@
 package projectcomposi133.entities;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Client2 {
+
+    DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+
+    public Client2(){
+
+    }
+
+    public Client2(String name, String email, LocalDate birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString(){
+        return name + " (" + fmt1.format(birthDate) + ") - " + email;
+
+    }
 }
