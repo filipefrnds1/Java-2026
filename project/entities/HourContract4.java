fc15ed585936c98f03d4af6175b048565ac01dbb
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-21 18:28:28 -0300

    HourContract4 Listas e Enum

--- project/entities/HourContract4.java
+++ project/entities/HourContract4.java
@@ -6,5 +6,45 @@ public class HourContract4 {
 
     private LocalDate date;
     private Double valuePerHour;
-    private 
+    private Integer hours;
+
+    public HourContract4(){
+
+    }
+
+    public HourContract4(LocalDate date, Double valuePerHour, Integer hours){
+        this.date = date;
+        this.valuePerHour = valuePerHour;
+        this.hours = hours;
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
+
+    public void setDate(LocalDate date){
+        this.date = date;
+    }
+
+    public Double getValuePerHour(){
+        return valuePerHour;
+    }
+
+    public void setValuePerHour(Double valuePerHour){
+        this.valuePerHour = valuePerHour;
+    }
+
+    public Integer getHours(){
+        return hours;
+    }
+
+    public void setHours(Integer hours){
+        this.hours = hours;
+    }
+
+    public double totalValue(){
+        return valuePerHour * hours;
+    }
+
+
 }
