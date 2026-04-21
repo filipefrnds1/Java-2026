80c46c0989967dff10413df5590747d8ec0f2e19
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-21 17:16:20 -0300

    Department4 Listas

--- project/entities/Department4.java
+++ project/entities/Department4.java
@@ -1,4 +1,26 @@
 package project.entities;
 
 public class Department4 {
+
+    private String name;
+
+    public Department4(){
+
+    }
+
+    public Department4(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String toString(){
+        return getName();
+    }
 }
