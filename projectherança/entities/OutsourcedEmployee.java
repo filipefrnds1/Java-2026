f3281289ffec40325bb46692c89a64d270c73ef3
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-24 10:15:32 -0300

    OutsourceEmp Exer142 poli

--- projectherança/entities/OutsourcedEmployee.java
+++ projectherança/entities/OutsourcedEmployee.java
@@ -1,4 +1,34 @@
 package projectherança.entities;
 
-public class OutsourcedEmployee {
+public class OutsourcedEmployee extends Employee {
+
+    private Double additionalCharge;
+
+    public OutsourcedEmployee() {
+    }
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public double payment(){
+        return hours * valuePerHour + 200;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(getName()).append(" - $ ").append(String.format("%.2f", payment()));
+        return sb.toString();
+    }
 }
