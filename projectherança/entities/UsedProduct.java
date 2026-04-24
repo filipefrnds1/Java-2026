7a32728152dcf6ac2295a54658f70dce5130f8eb
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-24 20:15:08 -0300

    Exer143 Polimorfismo UserdProd

--- projectherança/entities/UsedProduct.java
+++ projectherança/entities/UsedProduct.java
@@ -3,17 +3,17 @@ package projectherança.entities;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class UserProduct extends Product{
+public class UsedProduct extends Product{
 
     private LocalDate manufactureDate;
 
     public DateTimeFormatter ftm1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    public UserProduct() {
+    public UsedProduct() {
         super();
     }
 
-    public UserProduct(String name, Double price, LocalDate manufactureDate) {
+    public UsedProduct(String name, Double price, LocalDate manufactureDate) {
         super(name, price);
         this.manufactureDate = manufactureDate;
     }
@@ -29,7 +29,7 @@ public class UserProduct extends Product{
     @Override
     public String priceTag(){
         StringBuilder sb = new StringBuilder();
-        sb.append(getName()).append(" (used) $ ").append(String.format("%.2f",getPrice())).append(" (Manufacture date: ").append(getManufactureDate().format(ftm1));
+        sb.append(getName()).append(" (used) $ ").append(String.format("%.2f",getPrice())).append(" (Manufacture date: ").append(getManufactureDate().format(ftm1)).append(")");
         return sb.toString();
     }
 }
