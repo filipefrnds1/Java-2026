6ab4acac50c7358db41a180f09acaf42890c626f
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-24 20:14:19 -0300

    Exer143 Polimorfismo ImportedProd

--- projectherança/entities/ImportedProduct.java
+++ projectherança/entities/ImportedProduct.java
@@ -1,4 +1,34 @@
 package projectherança.entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product{
+
+    private Double customsFee;
+
+    public ImportedProduct(){
+        super();
+    }
+
+    public ImportedProduct(String name, Double price, Double customsFee){
+        super(name,price);
+        this.customsFee = customsFee;
+    }
+
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    @Override
+    public String priceTag(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(getName()).append(" $ ").append(String.format("%.2f",totalPrice())).append(" (Customs fee: $ ").append(getCustomsFee()).append(")");
+        return sb.toString();
+    }
+
+    public double totalPrice(){
+        return getPrice() + getCustomsFee();
+    }
 }
