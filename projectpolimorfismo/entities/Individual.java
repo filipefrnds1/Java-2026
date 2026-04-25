9419e88dbc185255ebf827fad2f44908c96ea769
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-25 15:53:17 -0300

    Individual Exer146 Polim and Abstract

--- projectpolimorfismo/entities/Individual.java
+++ projectpolimorfismo/entities/Individual.java
@@ -1,4 +1,36 @@
 package projectpolimorfismo.entities;
 
-public class Individual {
+public class Individual extends TaxPayer{
+
+    private Double healthExpend;
+
+    public Individual(){
+        
+    }
+
+    public Individual(String name, Double anuIncome, Double healthExpend) {
+        super(name, anuIncome);
+        this.healthExpend = healthExpend;
+    }
+
+    public Double getHealthExpend() {
+        return healthExpend;
+    }
+
+    public void setHealthExpend(Double healthExpend) {
+        this.healthExpend = healthExpend;
+    }
+
+    @Override
+    public double taxesPaid(){
+
+        if (getAnuIncome() >= 20000.00) {
+            return (getAnuIncome() * 0.25) - (getHealthExpend()*0.50);
+
+        }
+        else {
+            return (getAnuIncome() * 0.15) - (getHealthExpend()*0.50);
+        }
+
+    }
 }
