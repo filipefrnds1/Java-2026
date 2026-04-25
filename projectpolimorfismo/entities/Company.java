31a12d713a35fbb3dae3e5320ff5d9b98308e09a
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-25 15:52:39 -0300

    Company Exer146 Polim and Abstract

--- projectpolimorfismo/entities/Company.java
+++ projectpolimorfismo/entities/Company.java
@@ -1,4 +1,35 @@
 package projectpolimorfismo.entities;
 
-public class Company {
+public class Company extends TaxPayer{
+
+    private Integer employee;
+
+    public Company(){
+
+    }
+
+    public Company(String name, Double anuIncome, Integer employee) {
+        super(name, anuIncome);
+        this.employee = employee;
+    }
+
+    public Integer getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Integer employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public double taxesPaid() {
+
+        if(getEmployee() > 10){
+            return getAnuIncome()*0.14;
+        }
+        else{
+            return getAnuIncome()*0.16;
+        }
+
+    }
 }
