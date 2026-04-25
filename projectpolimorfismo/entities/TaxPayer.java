508b036942c0595df022d4298fdba1fcdda2a008
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-25 15:53:49 -0300

    TaxPayer Exer146 Polim and Abstract

--- projectpolimorfismo/entities/TaxPayer.java
+++ projectpolimorfismo/entities/TaxPayer.java
@@ -1,4 +1,34 @@
 package projectpolimorfismo.entities;
 
-public class TaxPayer {
+public abstract class TaxPayer {
+
+    private String name;
+    private Double anuIncome;
+
+    public TaxPayer(){
+
+    }
+
+    public TaxPayer(String name, Double anuIncome) {
+        this.name = name;
+        this.anuIncome = anuIncome;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAnuIncome() {
+        return anuIncome;
+    }
+
+    public void setAnuIncome(Double anuIncome) {
+        this.anuIncome = anuIncome;
+    }
+
+    public abstract double taxesPaid();
 }
