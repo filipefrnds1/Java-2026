ecdbc87f26d0c09cddfebc0bd2a9c6ce99cce509
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-27 23:08:12 -0300

    Program Exceções

--- projecttrycatch/application/ProgramReservation.java
+++ projecttrycatch/application/ProgramReservation.java
@@ -1,4 +1,16 @@
 package projecttrycatch.application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ProgramReservation {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+        sc.close();
+    }
 }
