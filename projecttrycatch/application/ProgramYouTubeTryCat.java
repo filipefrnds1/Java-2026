4eb6da5de2856b5851098e445abadab5bd5bd063
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-29 15:58:04 -0300

    Program YouTube try catch

---
+++ projecttrycatch/application/ProgramYouTubeTryCat.java
@@ -0,0 +1,24 @@
+package projecttrycatch.application;
+
+import projecttrycatch.entities.Financiamento;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramYouTubeTryCat {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double valorTotal = 1000.0;
+        double entrada = 100.0;
+        int parcelas = 10;
+
+        Financiamento f = new Financiamento(valorTotal,entrada,parcelas);
+        System.out.println(f.prestacao());
+
+
+
+        sc.close();
+    }
+}
