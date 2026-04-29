3cdc42b69f03523e9db48ca6999decceea929252
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-29 16:12:57 -0300

    Finalizado

--- projecttrycatch/application/ProgramYouTubeTryCat.java
+++ projecttrycatch/application/ProgramYouTubeTryCat.java
@@ -14,17 +14,13 @@ public class ProgramYouTubeTryCat {
         double entrada = 100.0;
         int parcelas = 10;
 
-        if(entrada < valorTotal * 0.2){
-            System.out.println("A entrada deve ser pelo menos 20% do valor total");
 
-        }
-        else if (parcelas < 6) {
-            System.out.println("O número mínimo de parcelas deve ser 6");
-
-        }
-        else{
+        try{
             Financiamento f = new Financiamento(valorTotal,entrada,parcelas);
             System.out.println(f.prestacao());
+        }
+        catch (RuntimeException e){
+            System.out.println(e.getMessage());
         }
 
 
