5ea16fec0f1776a67c6c6c9c1c20b5f15be51bf1
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-29 17:58:21 -0300

    Classe MusicTest Exception

--- projetobeatbox/entities/MusicTest1.java
+++ projetobeatbox/entities/MusicTest1.java
@@ -1,4 +1,19 @@
 package projetobeatbox.entities;
 
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequencer;
+
 public class MusicTest1 {
+
+    public void play(){
+
+        try{
+            Sequencer sequencer = MidiSystem.getSequencer();
+            System.out.println("Sucessfully got a sequencer");
+        }
+        catch (MidiUnavailableException e){
+            System.out.println("Bummer");
+        }
+    }
 }
