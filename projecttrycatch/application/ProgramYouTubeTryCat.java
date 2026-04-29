3cbde7cac319541eb05df2ebc09c0295e304861f
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-29 16:05:09 -0300

    Prog YT TryCat

--- projecttrycatch/application/ProgramYouTubeTryCat.java
+++ projecttrycatch/application/ProgramYouTubeTryCat.java
@@ -14,8 +14,20 @@ public class ProgramYouTubeTryCat {
         double entrada = 100.0;
         int parcelas = 10;
 
-        Financiamento f = new Financiamento(valorTotal,entrada,parcelas);
-        System.out.println(f.prestacao());
+        if(entrada < valorTotal * 0.2){
+            System.out.println("A entrada deve ser pelo menos 20% do valor total");
+
+        }
+        else if (parcelas < 6) {
+            System.out.println("O número mínimo de parcelas deve ser 6");
+
+        }
+        else{
+            Financiamento f = new Financiamento(valorTotal,entrada,parcelas);
+            System.out.println(f.prestacao());
+        }
+
+
 
 
 
