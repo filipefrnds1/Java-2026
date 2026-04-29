958da34026a23745945c70232c9af5a4a67ba70d
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-29 16:13:35 -0300

    throw new implementado no Construtor

--- projecttrycatch/entities/Financiamento.java
+++ projecttrycatch/entities/Financiamento.java
@@ -7,6 +7,15 @@ public class Financiamento {
     public Integer parcelas;
 
     public Financiamento(Double valorTotal, Double entrada, Integer parcelas) {
+
+        if(entrada < valorTotal * 0.2){
+            throw new RuntimeException("A entrada deve ser pelo menos 20% do valor total");
+
+        }
+        else if (parcelas < 6) {
+            throw new RuntimeException("O número mínimo de parcelas deve ser 6");
+
+        }
         this.valorTotal = valorTotal;
         this.entrada = entrada;
         this.parcelas = parcelas;
