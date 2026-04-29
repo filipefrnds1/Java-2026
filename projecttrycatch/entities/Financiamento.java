e37f43fce0ec8d336e58f95ddf8097a27ed1d761
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-29 15:58:32 -0300

    Financiamento YT trycatch

--- projecttrycatch/entities/Financiamento.java
+++ projecttrycatch/entities/Financiamento.java
@@ -1,4 +1,18 @@
 package projecttrycatch.entities;
 
 public class Financiamento {
+
+    public Double valorTotal;
+    public Double entrada;
+    public Integer parcelas;
+
+    public Financiamento(Double valorTotal, Double entrada, Integer parcelas) {
+        this.valorTotal = valorTotal;
+        this.entrada = entrada;
+        this.parcelas = parcelas;
+    }
+
+    public double prestacao(){
+        return (valorTotal - entrada) / parcelas;
+    }
 }
