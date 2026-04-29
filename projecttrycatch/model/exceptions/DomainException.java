ce3984ff1084436b97c5b6772a2a3c8b1b57eedc
Filipe Fernandes <filipefirmino50@gmail.com>
2026-04-29 12:26:16 -0300

    Exceptions

--- projecttrycatch/model/exceptions/DomainException.java
+++ projecttrycatch/model/exceptions/DomainException.java
@@ -1,6 +1,9 @@
 package projecttrycatch.model.exceptions;
 
+import java.io.Serial;
+
 public class DomainException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public DomainException(String msg) {
