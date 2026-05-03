c1b8c45fcfa78157d9045ea3a9563ede0e917ab8
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-03 01:44:44 -0300

    ProgramExer158 trycatch correção teacher's correction

--- projecttrycatch/application/ProgramAccount.java
+++ projecttrycatch/application/ProgramAccount.java
@@ -10,28 +10,29 @@ public class ProgramAccount {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
 
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Holder: ");
+        String name = sc.nextLine();
+        System.out.print("Initial Balance: ");
+        double balance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account account = new Account(number,name,balance,withdrawLimit);
+
+        System.out.print("\nEnter amount for withdraw: ");
+
         try{
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter account data");
-            System.out.print("Number: ");
-            int number = sc.nextInt();
-            sc.nextLine();
-            System.out.print("Holder: ");
-            String name = sc.nextLine();
-            System.out.print("Initial Balance: ");
-            double balance = sc.nextDouble();
-            System.out.print("Withdraw limit: ");
-            double withdrawLimit = sc.nextDouble();
-
-            Account account = new Account(number,name,balance,withdrawLimit);
-
-            System.out.print("\nEnter amount for withdraw: ");
             account.withDraw(sc.nextDouble());
-
-            System.out.println("New balance : " + account.getBalance());
+            System.out.println("New balance : " + String.format("%.2f",account.getBalance()));
         }
         catch (DomainExceptionAccount e){
-            System.out.println("Withdraw error: " + e.getMessage());
+            System.out.println(e.getMessage());
         }
 
     }
