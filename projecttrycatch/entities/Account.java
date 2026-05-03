412a29dd0cd1f908af638ee3b46c0290c8ad5a24
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-03 01:34:16 -0300

    AccountExer158 trycatch i did

--- projecttrycatch/entities/Account.java
+++ projecttrycatch/entities/Account.java
@@ -1,4 +1,60 @@
 package projecttrycatch.entities;
 
+import projecttrycatch.model.exceptions.DomainExceptionAccount;
+
 public class Account {
+
+    private Integer number;
+    private String holder;
+    private Double balance;
+    private Double withdrawLimit;
+
+    public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public Double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+
+    public void deposit(Double amount){
+        balance += amount;
+    }
+
+    public void withDraw(Double amount){
+
+        if(amount > withdrawLimit){
+            throw new DomainExceptionAccount("The amount exceeds withdraw limit");
+        }
+        if(amount > balance){
+            throw new DomainExceptionAccount("Not enough balance");
+        }
+
+        balance -= amount;
+    }
+
+
 }
