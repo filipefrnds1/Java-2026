81df90af4fc2e6096d6cb8e389ce1d3aada59a3c
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-02 23:57:22 -0300

    Reservation2B Bom

--- projecttrycatch/model/entities/Reservation2B.java
+++ projecttrycatch/model/entities/Reservation2B.java
@@ -1,4 +1,66 @@
 package projecttrycatch.model.entities;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Reservation2B {
+    private Integer roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    private static DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation2B(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration(){
+        return ChronoUnit.DAYS.between(checkIn,checkOut);
+    }
+
+    public void updateDates(LocalDate checkIn,LocalDate checkOut){
+        if(checkIn.isBefore(LocalDate.now()) || checkOut.isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("Reservation dates for update must be future dates");
+        }
+        if(!checkOut.isAfter(checkIn)){
+            throw new IllegalArgumentException("Error in reservation: Check-out date must be after check-in date");
+        }
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+
+    }
+
+    @Override
+    public String toString(){
+        return "Room " +
+                roomNumber +
+                ", check-in: " +
+                checkIn.format(fmt1) +
+                ", check-out: " +
+                checkOut.format(fmt1) +
+                ", " +
+                duration() +
+                " nights";
+
+
+    }
 }
