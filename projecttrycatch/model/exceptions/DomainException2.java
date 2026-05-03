1d9e7734ea08f45fe4875ebefc8ae7dd5f436045
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-03 00:47:56 -0300

    DomainException2 Bom Finalizado

--- projecttrycatch/model/exceptions/DomainException2.java
+++ projecttrycatch/model/exceptions/DomainException2.java
@@ -1,4 +1,13 @@
 package projecttrycatch.model.exceptions;
 
-public class DomainException2 {
+import java.io.Serial;
+
+public class DomainException2 extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DomainException2(String msg){
+        super(msg);
+    }
 }
