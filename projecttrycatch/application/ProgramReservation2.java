7f5ae5b2f557d2a7b3f9190227755d689d390699
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-02 23:22:59 -0300

    ProgramReservation2 trycatch

--- projecttrycatch/application/ProgramReservation2.java
+++ projecttrycatch/application/ProgramReservation2.java
@@ -1,4 +1,46 @@
 package projecttrycatch.application;
 
+import projecttrycatch.model.entities.Reservation2;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ProgramReservation2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        LocalDate checkIn = LocalDate.parse(sc.next(),fmt1);
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        LocalDate checkOut = LocalDate.parse(sc.next(),fmt1);
+
+        if(!checkOut.isAfter(checkIn)){
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        }
+        else{
+            Reservation2 reservation = new Reservation2(number,checkIn,checkOut);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkIn = LocalDate.parse(sc.next(),fmt1);
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkOut = LocalDate.parse(sc.next(),fmt1);
+
+            reservation.updateDates(checkIn,checkOut);
+            System.out.println("Reservation: " + reservation);
+
+        }
+
+
+        sc.close();
+
+    }
 }
