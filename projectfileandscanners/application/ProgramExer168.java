83de67795a52492c4c8eee7c2db7f1269f553eb1
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-05 02:51:32 -0300

    ProgramExer168

--- projectfileandscanners/application/ProgramExer168.java
+++ projectfileandscanners/application/ProgramExer168.java
@@ -1,4 +1,11 @@
 package projectfileandscanners.application;
 
+import java.util.Locale;
+
 public class ProgramExer168 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+
+    }
 }
