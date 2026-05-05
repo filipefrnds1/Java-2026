ca93dc8a728d731e7bbf8a0d55e4d30059f33128
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-05 01:25:18 -0300

    Caminho do arquivo e do diretorio

---
+++ projectfileandscanners/application/ProgramPathFile.java
@@ -0,0 +1,25 @@
+package projectfileandscanners.application;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramPathFile {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter file path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        System.out.println("getName: " + path.getName());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getPath: " + path.getPath());
+
+
+        sc.close();
+    }
+}
