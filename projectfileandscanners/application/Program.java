2a5c66c6b5b41390b1b37361fb44c7ff3b2283cb
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-04 23:47:25 -0300

    Programa para Ler Arquivos - File e Scanner

--- projectfileandscanners/application/Program.java
+++ projectfileandscanners/application/Program.java
@@ -1,4 +1,32 @@
 package projectfileandscanners.application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        File file = new File("c:\\temp\\in.txt");
+
+        Scanner sc = null;
+
+        try{
+            sc = new Scanner(file);
+            while (sc.hasNextLine()){ // verifica se ainda existe uma nova linha no arquivo
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            if(sc != null) {
+                sc.close();
+            }
+        }
+
+    }
 }
