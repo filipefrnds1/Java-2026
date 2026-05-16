2240783d587ace743aee28956d0d4ed540c7d567
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-16 01:50:54 -0300

    Department5 ENUM COMPOSI Exer130

--- project/entities/Department5.java
+++ project/entities/Department5.java
@@ -1,4 +1,21 @@
 package project.entities;
 
 public class Department5 {
+
+    private String name;
+
+    public Department5() {
+    }
+
+    public Department5(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
