329d3f6ab60a8d4e06bc72300110ed98083bd775
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-16 00:12:54 -0300

    Program4 Final ENUM COMPOSI

--- projectcomposi133/application/Program4.java
+++ projectcomposi133/application/Program4.java
@@ -1,8 +1,13 @@
 package projectcomposi133.application;
 
+import projectcomposi133.entities.Client4;
+import projectcomposi133.entities.Order4;
+import projectcomposi133.entities.OrderItem4;
+import projectcomposi133.entities.Product4;
 import projectcomposi133.enums.OrderStatus4;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
@@ -26,7 +31,35 @@ public class Program4 {
         System.out.print("Status: ");
         OrderStatus4 status = OrderStatus4.valueOf(sc.nextLine());
 
-        
+        Client4 client = new Client4(name,email,dt1);
+
+        Order4 order = new Order4(LocalDateTime.now(),status,client);
+
+        System.out.print("How many items to this order? ");
+        int qnItems = sc.nextInt();
+
+
+        for(int i = 0; i < qnItems; i++){
+
+            sc.nextLine();
+            System.out.println("Enter #" + (i+1) + " item data: ");
+            System.out.print("Product name: ");
+            String prodName = sc.nextLine();
+            System.out.print("Product price: ");
+            double prodPrice = sc.nextDouble();
+            System.out.print("Quantity: ");
+            int prodQuantity = sc.nextInt();
+
+
+            Product4 prod = new Product4(prodName,prodPrice);
+
+            OrderItem4 orderItem = new OrderItem4(prodQuantity,prodPrice,prod);
+
+            order.addItem(orderItem);
+
+        }
+
+        System.out.println(order);
 
 
 
