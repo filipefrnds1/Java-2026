1eba60d495644d9978ffa0820f40905c54130772
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-15 23:55:58 -0300

    OrderItem4 ENUM COMPOSI

--- projectcomposi133/entities/OrderItem4.java
+++ projectcomposi133/entities/OrderItem4.java
@@ -1,4 +1,52 @@
 package projectcomposi133.entities;
 
 public class OrderItem4 {
+    private Integer quantity;
+    private Double price;
+    private Product4 prod;
+
+    public OrderItem4() {
+    }
+
+    public OrderItem4(Integer quantity, Double price, Product4 prod) {
+        this.quantity = quantity;
+        this.price = price;
+        this.prod = prod;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Product4 getProd() {
+        return prod;
+    }
+
+    public void setProd(Product4 prod) {
+        this.prod = prod;
+    }
+
+    public Double subTotal(){
+        return quantity * price;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(prod.getName()).append(", $").append(String.format("%.2f",getPrice())).append(", Quantity: ")
+                .append(getQuantity()).append(", Subtotal: $").append(String.format("%.2f \n", subTotal()));
+        return sb.toString();
+    }
 }
