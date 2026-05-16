3840d58f1afe845eb9618e96a09adb1e22bc8fae
Filipe Fernandes <filipefirmino50@gmail.com>
2026-05-15 23:55:16 -0300

    Client4 ENUM COMPOSI

--- projectcomposi133/entities/Client4.java
+++ projectcomposi133/entities/Client4.java
@@ -1,4 +1,51 @@
 package projectcomposi133.entities;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Client4 {
+
+    DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+
+    public Client4(String name, String email, LocalDate birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(getName()).append(" (").append(fmt1.format(getBirthDate())).append(") - ").append(getEmail())
+                .append("\n");
+        return sb.toString();
+    }
 }
